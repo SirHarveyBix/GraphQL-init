ae221e8ffb6661adbb1892e3b6684403e3f2acf9
Guillaume <Gui.lefort.17@gmail.com>
2021-08-22 21:59:43 +0200

    advancing a litlle bit with arrays

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,14 +1,10 @@
 import { gql } from 'graphql-modules';
-//scalar types : String, ID, Boolean, Int, Float // ! => must be filled
 
-//Schema
 export const typeDefs = gql`
   type Query {
-    greeting(name: String!, pos: String): String!
-    add(numbers: [Float!]!): Float!
-    grades: [Int]!
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
     me: User!
-    post: Post!
   }
   type User {
     id: ID!
