88315a7d7b8e75e39dea041c08bd1a300f501321
Guillaume <Gui.lefort.17@gmail.com>
2021-08-23 09:49:24 +0200

    int relation with users and posts + add comments

--- src/typeDefs.js
+++ src/typeDefs.js
@@ -4,18 +4,24 @@ export const typeDefs = gql`
   type Query {
     users(query: String): [User!]!
     posts(query: String): [Post!]!
-    me: User!
+    comments(query: String): [Comment!]!
   }
   type User {
     id: ID!
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
   type Post {
     id: ID!
     title: String!
     body: String!
     published: Boolean!
+    author: User!
+  }
+  type Comment {
+    id: ID!
+    textField: String!
   }
 `;
