774ee2be0a04ae55846c178f89d68306eaa6f667
Guillaume <Gui.lefort.17@gmail.com>
2021-08-24 15:40:54 +0200

    init subscription with pub sub

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 import db from './data/db';
 import Query from './resolvers/Query';
 import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
 import User from './resolvers/User';
 import Comment from './resolvers/Comment';
 import Post from './resolvers/Post';
+
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: './src/typeDefs.graphql',
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment,
   },
-  context: { db },
+  context: { db, pubsub },
 });
 
 server.start(() => {
